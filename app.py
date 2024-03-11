7abc36a6922d06f26569b08a08b15759a3b899cf
Haig Bedros <haigbedros@gmail.com>
2024-03-11 13:55:06 -0400

    config json and running HL scan CLI

--- app.py
+++ app.py
@@ -7,54 +7,54 @@ from subprocess import run, CalledProcessError
 
 app = Flask(__name__)
 
-# Load the configuration from a JSON file or environment variable
+# Load the configuration from a JSON file
 with open('repo_config.json') as config_file:
     config = json.load(config_file)
 BASE_TARGET_DIR = config['base_target_dir']
-# WEBHOOK_SECRET = os.getenv('GITHUB_WEBHOOK_SECRET') # to set global variable
+HIGHLIGHT_JAR_PATH = config['highlight_jar_path']
+WORKING_DIR = config['working_dir']
+PERL_INSTALL_DIR = config['perl_install_dir']  # Load the Perl installation directory
 
 # Hardcoding the WEBHOOK_SECRET for troubleshooting
-WEBHOOK_SECRET = ''
+WEBHOOK_SECRET = 'w6uHzbeW4ZKMkfVShdj8rR'
 
 def clone_repository(repo_url, target_dir):
     try:
-        # Clone the repository using the 'git clone' command
         run(['git', 'clone', repo_url, target_dir], check=True)
         print(f'Repository cloned successfully into {target_dir}')
+        execute_cli_command(target_dir)
     except CalledProcessError as e:
         print(f'Failed to clone repository: {e}')
 
-def verify_signature(request):
-    
-    #if WEBHOOK_SECRET is None:
-    #    print("WEBHOOK_SECRET is not set.")
-    #    abort(500, "Server configuration error.")
-    #    print(f"WEBHOOK_SECRET: {WEBHOOK_SECRET}")
+def execute_cli_command(target_dir):
+    try:
+        command = [
+            'java', '-jar', HIGHLIGHT_JAR_PATH, 
+            '--workingDir', WORKING_DIR, 
+            '--sourceDir', target_dir,
+            '--perlInstallDir', PERL_INSTALL_DIR,  # Perl
+            '--skipUpload'
+        ]
+        run(command, check=True)
+        print('CLI command executed successfully')
+    except CalledProcessError as e:
+        print(f'Failed to execute CLI command: {e}')
 
-    # Get the signature from the request headers
+def verify_signature(request):
     signature = request.headers.get('X-Hub-Signature')
-    
-    # Compute the HMAC hex digest
     hmac_digest = hmac.new(WEBHOOK_SECRET.encode(), request.data, hashlib.sha1).hexdigest()
-    
-    # Verify if the computed digest matches the provided signature
     return hmac.compare_digest(signature, 'sha1=' + hmac_digest)
 
 @app.route('/webhook', methods=['POST'])
 def handle_webhook():
-    # First, verify the webhook signature
     if not verify_signature(request):
         abort(400, 'Invalid signature')
 
-    # Parse the webhook payload
     data = request.json
-    repo_url = data['repository']['clone_url']  # Get the clone URL from the webhook payload
-    repo_name = data['repository']['name']  # Get the repository name
-
-    # Construct the target directory path
+    repo_url = data['repository']['clone_url']
+    repo_name = data['repository']['name']
     target_dir = os.path.join(BASE_TARGET_DIR, repo_name)
 
-    # Clone the repository
     clone_repository(repo_url, target_dir)
 
     return 'Webhook processed', 200
